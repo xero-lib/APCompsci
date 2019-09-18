b28aec384251a4509beafb951d8e8b5b7b78e56b
Jonathan K <54485853+xero-lib@users.noreply.github.com>
2019-09-18 12:42:11 -0500

    Add files via upload

---
+++ PayrollLoop.java
@@ -0,0 +1,67 @@
+//NAME:  Ms. Griffin
+
+import java.util.Scanner;
+
+
+public class PayrollLoop
+{
+    public static void main(String[] args) 
+    {
+    // Prepare to receive output
+    Scanner scan = new Scanner(System.in);
+    
+    // initialize variables
+    int numberEmployees = 0;
+    double totalGrossPay = 0;
+    double averagePay = 0;
+    
+    Payroll employee = new Payroll();
+    String repeat = "yes";
+    
+    // Loop to ask for repeated entry of employees.
+    // New String method: stringObject.equals(anotherStringObject). 
+    // This method returns a boolean value of True or False.
+    
+    while ( repeat.equals("yes") )	
+    {
+    	// prompt and scan for employee name    	
+    	
+    	
+    	// prompt and scan for ID number
+    	
+    	
+    	// prompt and scan for pay rate
+    	
+    	
+    	// prompt and scan for hours worked
+    	
+    	
+    	// what should you do to the numberEmployees variable here?
+    	
+    	
+    	// what should you do to the totalGrossPay variable here?
+    	
+    	
+    	// use the getter methods to print all the employee values
+    	
+    	
+    	// Ask user if entering another and scan for response
+    	
+    	
+    	
+    } // end of the while loop	
+    	
+    	
+    	
+    // calculate average pay
+    
+    
+    
+    // print average pay
+    
+    
+    
+    
+    	    	
+    } // end of main method    
+} // end of class (the whole file)
---
+++ PayrollTest.java
@@ -0,0 +1,50 @@
+//NAME:  Your Name here
+
+import java.util.Scanner;
+
+public class PayrollTest
+{
+    public static void main(String[] args) 
+    {
+     	
+     	/*
+     	 *  To run this program, you must have the Payroll.class file
+     	 *  in the same folder as the PayrollTest.java file.
+     	 */
+     	
+     	// prepare to receive input
+     	
+    	
+    	// Create two Payroll objects
+    	
+    	
+    	// Set attributes of the first object
+    	
+    	
+    	// Set attributes of the second object
+    	
+    	
+    	// View the objects using the toString method that is included in the class
+    	System.out.println();
+		System.out.println();
+    	System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+    	
+    	
+    	// Call all of the GET methods to print data in a nicer format.
+    	// Do this for both objects.
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+
+  
+          
+    } // this brace ends the main method
+    
+} // this brace ends the class.
